9d2f7cf3f6e70fde6762f002370c8609b3c8e5e7
Felix Krause <felix_krause1999@yahoo.de>
2023-02-19 19:05:52 +0100

    small fixes

--- kod/utils/__init__.py
+++ kod/utils/__init__.py
@@ -3,6 +3,28 @@
 utils/initialization
 """
 
+import contextlib
+import platform
+
+
+def emojis(str=''):
+    # Return platform-dependent emoji-safe version of string
+    return str.encode().decode('ascii', 'ignore') if platform.system() == 'Windows' else str
+
+
+class TryExcept(contextlib.ContextDecorator):
+    # YOLOv5 TryExcept class. Usage: @TryExcept() decorator or 'with TryExcept():' context manager
+    def __init__(self, msg=''):
+        self.msg = msg
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, value, traceback):
+        if value:
+            print(emojis(f"{self.msg}{': ' if self.msg else ''}{value}"))
+        return True
+
 
 def notebook_init(verbose=True):
     # Check system software and hardware
